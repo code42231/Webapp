fecd70c52d28c983aa5d28c044c4e09f6b3f6536
Ambraxil <potatololhi@gmail.com>
2026-08-18 21:12:33 -0600

    removed upload page and nav since there's no other pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,13 @@
 // App.jsx
-import { useEffect, useState } from "react";
-import Nav from "./Nav.jsx";
 import ViewerPage from "./ViewerPage.jsx";
-import UploadPage from "./UploadPage.jsx";
-
-function getPage() {
-  return window.location.hash === "#/upload" ? "upload" : "viewer";
-}
 
 export default function App() {
-  const [page, setPage] = useState(getPage());
-
-  useEffect(() => {
-    const onHashChange = () => setPage(getPage());
-    window.addEventListener("hashchange", onHashChange);
-    return () => window.removeEventListener("hashchange", onHashChange);
-  }, []);
-
   return (
     <>
       <header>
         <h1>Glyph</h1>
       </header>
-      <Nav page={page} />
-      {page === "viewer" ? <ViewerPage /> : <UploadPage />}
+      <ViewerPage />
     </>
   );
 }
